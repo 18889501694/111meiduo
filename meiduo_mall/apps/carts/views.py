ba338aa6c7b2fac092ca3550ffe21e348b3f53b5
HP <1179463172@qq.com>
2022-04-07 15:11:37 +0800

    对购物车的新增的代码完善

--- meiduo_mall/apps/carts/views.py
+++ meiduo_mall/apps/carts/views.py
@@ -17,10 +17,12 @@ class CartsView(View):
         """
         1、接收数据  2、验证数据
         3、判断用户的登录状态（request.user关联User的模型数据 用is_authenticated=True来验证用户 False是匿名用户）
-        4、登录用户购物车保存redis（1、连接redis 2、操作hash 3、操作set 4、返回响应）
-            redis中hash用法 :redis_cli.hset(key,field,value)  set的用法:redis_cli.sadd(key,field)
-        5、未登录用户保存cookie（1、先有cookie字典 2、字典转换为bytes 3、bytes类型数据base64编码 4、设置cookie 5、返回响应）
-            base64encode.decode()的作用是将bytes类型转换为str
+
+        4、登录用户购物车保存redis：1、连接redis 2、操作hash 3、操作set 4、返回响应
+                                redis中hash用法 :redis_cli.hset(key,field,value)  set的用法:redis_cli.sadd(key,field)
+
+        5、未登录用户保存cookie：1、先读取cookie数据进行判断   2、先有cookie字典    3、字典转换为bytes
+                    4、bytes类型数据base64编码(base64encode.decode()的作用是将bytes类型转换为str) 5、设置cookie  6、返回响应
         """
         data = json.loads(request.body.decode())
         sku_id = data.get('sku_id')
@@ -44,9 +46,18 @@ class CartsView(View):
             redis_cli.add('selected_%s' % user.id, sku_id)
             return JsonResponse({'code': 0, 'errmsg': 'ok'})
         else:
-            carts = {
-                sku_id: {'count': count, 'selected': True}
-            }
+            cookie_carts = request.COOKIES.get('carts')
+            if cookie_carts:
+                carts = pickle.loads(base64.b64decode(cookie_carts))
+            else:
+                carts = {}
+
+            if sku_id in carts:
+                # 购物车中已经有商品id 数量累加
+                origin_count = carts[sku_id]['count']
+                count += origin_count
+
+            carts[sku_id] = {'count': count, 'selected': True}
             carts_bytes = pickle.dumps(carts)
             base64encode = base64.b64encode(carts_bytes)
             response = JsonResponse({'code': 0, 'errmsg': 'ok'})
